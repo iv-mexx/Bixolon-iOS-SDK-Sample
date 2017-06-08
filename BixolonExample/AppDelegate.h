f793502bff73262667bac1ac4f1350da1127e582
Markus Chmelar <mexx@innovaptor.com>
2017-06-08 15:19:00 +0200

    Initial Commit

---
+++ BixolonExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BixolonExample
+//
+//  Created by Markus Chmelar on 08/06/2017.
+//  Copyright © 2017 Innovaptor OG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BixolonExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BixolonExample
+//
+//  Created by Markus Chmelar on 08/06/2017.
+//  Copyright © 2017 Innovaptor OG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
